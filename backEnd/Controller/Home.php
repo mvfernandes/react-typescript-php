7831807441db45006e660b5c0f4a75e785ef204f
Vinicius <solutiontecforyou@gmail.com>
2021-06-03 12:11:15 -0300

    reafactor require BaseController in Controllers

--- backEnd/Controller/Home.php
+++ backEnd/Controller/Home.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . "/Controller.php";
 class Home extends Controller
 {
     public function __construct($protected = false) {
--- backEnd/Controller/Menu.php
+++ backEnd/Controller/Menu.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . "/Controller.php";
+
 class Menu extends Controller
 {
     public function __construct($protected = false) {
